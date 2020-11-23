f5a39e905296b999d8f80cc6b9b4cafd700ba65b
Daniel Justesen <Daniel@justesen.biz>
2020-11-23 12:48:34 +0100

    small bug fixes

--- Assets/Scripts/Enemy/EnemyChasePlayer.cs
+++ Assets/Scripts/Enemy/EnemyChasePlayer.cs
@@ -8,7 +8,7 @@ public class EnemyChasePlayer : MonoBehaviour
     public float maxSpeed;
     public Transform player;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
